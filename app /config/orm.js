9ecf9beff7b0b587ea8602bc037d2e7cf38722b3
leslieo23 <leslie.obour23@gmail.com>
2016-05-10 18:31:00 -0400

    another commit

--- app /config/orm.js
+++ app /config/orm.js
@@ -19,7 +19,7 @@ var orm = {
 	},
 
 	
-	searchCharacter: function(name, callback){
+	searchBurger: function(name, callback){
 		var s = 'select * from ' + burgers_db + ' where routeName=?';
 
 		connection.query(s,[name], function(err, result) {
@@ -29,12 +29,9 @@ var orm = {
 
 	},
 
-	// Here our ORM is creating a simple method for adding characters to the database
-	// Effectively, the ORM's simple addCharacter method translates into a more complex SQL INSERT statement. 
 
-	addCharacter: function(character, callback){
+	addBurger: function(character, callback){
 
-		// Creating a routeName so its easy to search. 
 		var routeName = burgers_db.name.replace(/\s+/g, '').toLowerCase();
 		console.log(routeName);
 
